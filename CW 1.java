d3dd098ccf1be0b3f44d09702e36c9abde0492ac
Namareq Alkout <namareq505@gmail.com>
2022-09-05 19:45:02 +0300

    all done

---
+++ CW 1.java
@@ -0,0 +1,13 @@
+ String name = "Namareq"; 
+         int age = 18; 
+         
+         System.out.println( "Hello my name is " + name + " and i am " + age + " years old"); 
+         
+         int height = 160; 
+         
+         if (height>=160) {
+             System.out.println( "You can enter the game");
+         } 
+         else{
+             System.out.println("You can't enter the game");
+         }
